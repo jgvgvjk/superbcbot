74049006d59a99aaf86e9863d408a03c0592bba9
Modo <39749105+ImHamoood@users.noreply.github.com>
2018-06-26 13:50:15 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+onst Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('---premiumbc')){
+ if(!message.author.id === '411911292643704854') return;
+message.channel.sendMessage(' جار ارسال الرسالة | ✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN)
