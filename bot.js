97176050c8b9b73ec6fe919bea37379e96b2de19
Modo <39749105+ImHamoood@users.noreply.github.com>
2018-06-27 19:12:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1419,6 +1419,20 @@ Server owner: __${guild.owner}__**`)
 });
 
 
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('---premiumbc')){
+ if(!message.author.id === '411911292643704854') return;
+message.channel.sendMessage(' جار ارسال الرسالة | ✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
 
 
 
